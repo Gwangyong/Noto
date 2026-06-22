00af3a1b8d255bc56011cf38d6130f825577af1f
Gwangyong <key4168@icloud.com>
2026-06-22 17:34:48 +0900

    fix(settings): 완료 효과음 재생 방식 개선 - 커스텀 완료 효과음 적용 - 재생 중 다시 체크하면 기존 소리를 끊고 처음부터 재생

--- Noto/Services/CompletionSoundService.swift
+++ Noto/Services/CompletionSoundService.swift
@@ -7,13 +7,25 @@ import AppKit
 import Foundation
 
 final class CompletionSoundService {
+    private let sound: NSSound? = {
+        if let soundURL = Bundle.main.url(forResource: "ping_custom", withExtension: "wav") {
+            return NSSound(contentsOf: soundURL, byReference: false)
+        }
+        return NSSound(named: NSSound.Name("Ping"))
+    }()
+
     func playIfEnabled(_ isEnabled: Bool) {
         guard isEnabled else { return }
 
-        if let sound = NSSound(named: NSSound.Name("Glass")) {
-            sound.play()
-        } else {
+        guard let sound else {
             NSSound.beep()
+            return
         }
+
+        if sound.isPlaying {
+            sound.stop()
+        }
+        sound.currentTime = 0
+        sound.play()
     }
 }
